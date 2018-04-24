70c359cf9b719315c279652cee959d8e5900b34e
cdiazarze <cdiaz1@miuandes.cl>
2018-04-24 15:22:42 -0300

    binary-tree-IsisilvaLaboratorio 7 - Isidora Silva

---
+++ Laboratorio 7 - Isidora Silva/Laboratorio 7/ArbolBinario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_7
+{
+    public class ArbolBinario
+    {
+        class Nodo
+        {
+            public int informacion;
+            public Nodo izquerda, derecha;
+        }
+        Nodo raiz;
+        public void Insertar(int informacion)
+        {
+            Nodo nuevo;
+            nuevo = new Nodo();
+            nuevo.informacion = informacion;
+            nuevo.izquerda = null;
+            nuevo.derecha = null;
+            if (raiz == null)
+                raiz = nuevo;
+            else
+            {
+                Nodo anterior = null, reco;
+                reco = raiz;
+                while (reco != null)
+                {
+                    anterior = reco;
+                    if (informacion < reco.informacion)
+                        reco = reco.izquerda;
+                    else
+                        reco = reco.derecha;
+                }
+                if (informacion < anterior.informacion)
+                    anterior.izquerda = nuevo;
+                else
+                    anterior.derecha = nuevo;
+            }
+        }
+    }
+}
---
+++ Laboratorio 7 - Isidora Silva/Laboratorio 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBinario arbol = new ArbolBinario();
+            arbol.Insertar(100);
+            arbol.Insertar(50);
+            arbol.Insertar(25);
+            arbol.Insertar(867);
+            arbol.Insertar(86);
+        }
+    }
+}
